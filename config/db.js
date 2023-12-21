41eb8c03eff2f50cebc38bf9a3e1950f59f0f875
kevinteixeira97 <126160737+kevinteixeira97@users.noreply.github.com>
2023-12-21 18:58:43 +0000

    nice

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb+srv://admin:<4QmkXoIoXLiCiOf9>@cluster0.50hiiro.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
-
+var connection = mongoose.connect('mongodb+srv://admin:4QmkXoIoXLiCiOf9@cluster0.50hiiro.mongodb.net/clinica_medica?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 module.exports = connection;
