d4bd8e1acd04aff2f4b004f1ff0689aea7f900e5
tribopower <50154079+tribopower@users.noreply.github.com>
2023-12-21 19:58:34 +0000

    Update medicos.js

--- routes/medicos.js
+++ routes/medicos.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
     });
 });
 
+
 router.get('/:id_medico', function(req, res) {
     var id_medico = req.params.id_medico; // Usando id_medico como parâmetro
     medico.findOne({ id_medico: id_medico }, function (err, data) {
